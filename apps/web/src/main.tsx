17a1a520c3fb5a4cf6593afaf8504b9739b3df9f
Franz Dittmann <635927+kajoik@users.noreply.github.com>
2026-02-23 21:45:07 +0100

    Remove vite's default content

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+const rootNode = document.getElementById("root");
+
+if (!rootNode) {
+  throw new Error("#root element not found.");
+}
+
+createRoot(rootNode).render(
   <StrictMode>
     <App />
   </StrictMode>,
