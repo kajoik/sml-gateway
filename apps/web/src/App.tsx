6088c2b90909667226af5cfadc238c4759ccba01
Franz Dittmann <635927+kajoik@users.noreply.github.com>
2026-02-23 23:08:42 +0100

    Add dashboard boilerplate

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,17 @@
+import AppTheme from "./theme/AppTheme";
+import SideMenu from "./components/SideMenu";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+
 function App() {
-  return <></>;
+  return (
+    <AppTheme>
+      <CssBaseline />
+      <Box sx={{ display: "flex" }}>
+        <SideMenu />
+      </Box>
+    </AppTheme>
+  );
 }
 
 export default App;
---
+++ apps/web/src/components/MenuContent.tsx
@@ -0,0 +1,50 @@
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Stack from "@mui/material/Stack";
+import HomeIcon from "@mui/icons-material/Home";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { ListItem } from "@mui/material";
+
+const mainListItems = [{ text: "Home", icon: <HomeIcon /> }];
+
+const secondaryListItems = [
+  {
+    text: "GitHub",
+    icon: <GitHubIcon />,
+    href: "https://github.com/kajoik/sml-gateway/",
+  },
+];
+
+export default function MenuContent() {
+  return (
+    <Stack sx={{ flexGrow: 1, p: 1, justifyContent: "space-between" }}>
+      <List dense>
+        {mainListItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: "block" }}>
+            <ListItemButton selected={index === 0}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <List dense>
+        {secondaryListItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              component="a"
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Stack>
+  );
+}
