19b08c7b5020acb45aab0f329f6fd176dec98305
Franz Dittmann <635927+kajoik@users.noreply.github.com>
2026-02-23 14:07:35 +0100

    Set Content-Type header to application/json

--- src/sml-stream/sml-stream.controller.ts
+++ src/sml-stream/sml-stream.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Header } from "@nestjs/common";
 import { SmlStreamService } from "./sml-stream.service";
 
 @Controller("sml-stream")
@@ -6,6 +6,7 @@ export class SmlStreamController {
   constructor(private readonly smlStreamService: SmlStreamService) {}
 
   @Get("/sample")
+  @Header("Content-Type", "application/json")
   getSample(): string {
     return JSON.stringify(this.smlStreamService.getSample());
   }
