4372243e0d9cef4485422602ae9e20ccba8d2d4e
Franz Dittmann <635927+kajoik@users.noreply.github.com>
2026-02-24 15:18:23 +0100

    Use better headline for live data table

--- apps/web/src/components/MainGrid.tsx
+++ apps/web/src/components/MainGrid.tsx
@@ -6,7 +6,7 @@ export default function MainGrid() {
   return (
     <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
       <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-        Overview
+        Live Data
       </Typography>
       <SampleTable />
     </Box>
