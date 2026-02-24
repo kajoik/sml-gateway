207270e83e4499c288e0d630963dfec69e3949dd
Franz Dittmann <635927+kajoik@users.noreply.github.com>
2026-02-24 15:13:09 +0100

    Switch to Data Grid
    The MUIX Data Grid has the ability to disable columns. I disabled some
    less relevant columns by default.

--- apps/web/src/components/SampleTable.tsx
+++ apps/web/src/components/SampleTable.tsx
@@ -1,24 +1,33 @@
 import { useEffect, useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import type { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import { io } from "socket.io-client";
+import CircularProgress from "@mui/material/CircularProgress";
+import Alert from "@mui/material/Alert";
+
+type ObisEntry = {
+  obisId: string;
+  mediumName?: string;
+  measurementName?: string;
+  measurementTypeName?: string;
+  tariffRateName?: string;
+  previousMeasurementName?: string;
+  customName?: string;
+  obisName?: string;
+  values?: unknown;
+};
 
 export default function SampleTable() {
-  // TODO: Replace the "any"
-  const [lastObisEntries, setLastObisEntries] = useState<any[]>([]);
+  const [rows, setRows] = useState<ObisEntry[]>([]);
 
   useEffect(() => {
     const socket = io();
 
     socket.emit("subscribeSamples");
 
-    socket.on("sample", (payload: { type: string; value: any[] }) => {
-      setLastObisEntries(payload.value);
+    socket.on("sample", (payload: { type: string; value: ObisEntry[] }) => {
+      setRows(payload.value);
     });
 
     return () => {
@@ -27,53 +36,81 @@ export default function SampleTable() {
     };
   }, []);
 
+  const columns: GridColDef[] = [
+    { field: "obisId", headerName: "Obis Id", width: 140 },
+    { field: "mediumName", headerName: "Medium Name", width: 140 },
+    { field: "measurementName", headerName: "Measurement Name", width: 180 },
+    {
+      field: "measurementTypeName",
+      headerName: "Measurement Type Name",
+      width: 200,
+    },
+    { field: "tariffRateName", headerName: "Tariff Rate Name", width: 160 },
+    {
+      field: "previousMeasurementName",
+      headerName: "Previous Measurement Name",
+      width: 200,
+    },
+    { field: "customName", headerName: "Custom Name", width: 140 },
+    { field: "obisName", headerName: "Obis Name", width: 340 },
+    {
+      field: "value",
+      headerName: "Value",
+      width: 300,
+      renderCell: (params: GridRenderCellParams) => {
+        if (params.row.values?.[0]) {
+          return `${params.row.values[0].value} ${params.row.values[0].unit}`;
+        } else {
+          return "-";
+        }
+      },
+      sortable: false,
+      filterable: false,
+    },
+    {
+      field: "values",
+      headerName: "Raw Values",
+      width: 300,
+      renderCell: (params: GridRenderCellParams) =>
+        JSON.stringify(params.value),
+      sortable: false,
+      filterable: false,
+    },
+  ];
+
+  const rowsWithId = rows.map((r) => ({ id: r.obisId, ...r }));
+
+  if (rows.length < 1) {
+    return (
+      <Box textAlign="center">
+        <CircularProgress />
+      </Box>
+    );
+  }
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Obis Id</TableCell>
-            <TableCell align="right">Medium Name</TableCell>
-            <TableCell align="right">Measurement Name</TableCell>
-            <TableCell align="right">Measurement Type Name</TableCell>
-            <TableCell align="right">Tariff Rate Name</TableCell>
-            <TableCell align="right">Previous Measurement Name</TableCell>
-            <TableCell align="right">Custom Name</TableCell>
-            <TableCell align="right">Obis Name</TableCell>
-            <TableCell align="right">Values</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {lastObisEntries.map((lastObisEntry) => (
-            <TableRow
-              key={lastObisEntry.obisId}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {lastObisEntry.obisId}
-              </TableCell>
-              <TableCell align="right">{lastObisEntry.mediumName}</TableCell>
-              <TableCell align="right">
-                {lastObisEntry.measurementName}
-              </TableCell>
-              <TableCell align="right">
-                {lastObisEntry.measurementTypeName}
-              </TableCell>
-              <TableCell align="right">
-                {lastObisEntry.tariffRateName}
-              </TableCell>
-              <TableCell align="right">
-                {lastObisEntry.previousMeasurementName}
-              </TableCell>
-              <TableCell align="right">{lastObisEntry.customName}</TableCell>
-              <TableCell align="right">{lastObisEntry.obisName}</TableCell>
-              <TableCell align="right">
-                {JSON.stringify(lastObisEntry.values)}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box>
+      <Alert severity="info" sx={{ mb: 2 }}>
+        Some columns are deactivated by default. You can activate them in the
+        column menu.
+      </Alert>
+      <DataGrid
+        rows={rowsWithId}
+        columns={columns}
+        initialState={{
+          columns: {
+            columnVisibilityModel: {
+              customName: false,
+              obisId: false,
+              previousMeasurementName: false,
+              values: false,
+            },
+          },
+        }}
+        hideFooter
+        autoHeight
+        disableRowSelectionOnClick
+      />
+    </Box>
   );
 }
